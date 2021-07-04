3e507ea63e85825acba140d28a7f6f1c72d6d203
Abhirup-99 <abhiruppalmethodist@gmail.com>
2021-07-04 12:44:38 +0530

    format: Add prettier

--- eos-icons/code.js
+++ eos-icons/code.js
@@ -1,18 +1,18 @@
 figma.showUI(__html__);
 figma.ui.onmessage = (msg) => {
-    if (msg.type === "search-icons") {
-        figma.ui.postMessage({
-            type: "get-svg",
-            searchText: msg.searchText,
-        });
-    }
-    if (msg.type === "handle-icon") {
-        const nodes = [];
-        const icon = figma.createNodeFromSvg(msg.svg);
-        icon.rescale(3);
-        nodes.push(icon);
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-        figma.closePlugin();
-    }
+  if (msg.type === "search-icons") {
+    figma.ui.postMessage({
+      type: "get-svg",
+      searchText: msg.searchText,
+    });
+  }
+  if (msg.type === "handle-icon") {
+    const nodes = [];
+    const icon = figma.createNodeFromSvg(msg.svg);
+    icon.rescale(3);
+    nodes.push(icon);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.closePlugin();
+  }
 };
