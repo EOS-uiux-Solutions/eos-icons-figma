3118f867067b9d053a60150e920078540e26fe63
Abhirup-99 <abhiruppalmethodist@gmail.com>
2021-08-13 13:58:33 +0530

    tests added.

---
+++ src/EosForm.test.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import EosForm from "./EosForm";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders the form", () => {
+  const searchCategory = jest.fn();
+  act(() => {
+    render(
+      <EosForm
+        inputField={{ current: null }}
+        handleKeyUp={() => {}}
+        onSearch={searchCategory}
+        searchTheme={{ current: null }}
+        searchCategory={{ current: null }}
+        clearValue={() => {}}
+      />,
+      container
+    );
+  });
+  const categoryInputField = document.querySelectorAll(".select-tag")[0];
+  act(() => {
+    categoryInputField.dispatchEvent(
+      new MouseEvent("change", { bubbles: true })
+    );
+  });
+
+  expect(searchCategory).toHaveBeenCalledTimes(1);
+  const themeInputField = document.querySelectorAll(".select-tag")[1];
+  act(() => {
+    themeInputField.dispatchEvent(new MouseEvent("change", { bubbles: true }));
+  });
+
+  expect(searchCategory).toHaveBeenCalledTimes(2);
+});
---
+++ src/IconBox.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import {
+  EOS_10MP_FILLED,
+  EOS_TYPING_ANIMATED,
+  EOS_10MP_OUTLINED,
+} from "eos-icons-react";
+import IconBox from "./IconBox";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders an iconbox", () => {
+  const pushToNode = jest.fn();
+  const eosReactIcons = [
+    { EOSReactIcon: EOS_10MP_FILLED, name: "10mp" },
+    { EOSReactIcon: EOS_TYPING_ANIMATED, name: "typing" },
+    { EOSReactIcon: EOS_10MP_OUTLINED, name: "10mpOutlined" },
+  ];
+  act(() => {
+    render(
+      <IconBox option="Action" icons={eosReactIcons} pushToNode={pushToNode} />,
+      container
+    );
+  });
+  expect(container.querySelectorAll(".image-container").length).toBe(3);
+  expect(
+    container.querySelector(".category-container span").textContent
+  ).toEqual("Action");
+  const iconDiv = document.querySelectorAll(".image-container")[0];
+  act(() => {
+    iconDiv.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(pushToNode).toHaveBeenCalledTimes(1);
+});
