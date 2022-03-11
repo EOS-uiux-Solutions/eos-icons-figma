4451da2d835b34c5e4e684d1df30a4582fa9b839
abhishek <abhisonuk@gmail.com>
2022-03-12 00:55:38 +0530

    fixed #18

--- src/code.ts
+++ src/code.ts
@@ -4,13 +4,30 @@ figma.showUI(__html__, {
   height: 600,
 });
 
+let prevLoc = figma.viewport.center.x;
+let prevViewX = figma.viewport.center.x;
+let prevViewY = figma.viewport.center.y;
+
+function onViewPortChanged(iconWidth: number) {
+  if (
+    prevViewX !== figma.viewport.center.x ||
+    prevViewY !== figma.viewport.center.y
+  ) {
+    // whether the scene is scrolled or not
+    prevLoc = figma.viewport.center.x;
+    prevViewX = figma.viewport.center.x;
+    prevViewY = figma.viewport.center.y;
+  } else prevLoc += iconWidth + spaceBtwIcon;
+}
+
+const spaceBtwIcon: number = 20;
 figma.ui.onmessage = (msg: { type: string; svg: any; name: string }) => {
   if (msg.type === "handle-icon") {
-    const nodes: SceneNode[] = [];
     const icon = figma.createNodeFromSvg(msg.svg);
     icon.name = msg.name;
-    nodes.push(icon);
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    onViewPortChanged(icon.width);
+    icon.x = prevLoc;
+    icon.y = figma.viewport.center.y;
+    figma.currentPage.selection = [icon];
   }
 };
