65427efc9501375695ca6ebfc8dde4ead1a6bb6c
abhishek <abhisonuk@gmail.com>
2022-03-18 12:26:24 +0530

    fix : fixed #23 refactor : removed usesless code

--- src/ui.tsx
+++ src/ui.tsx
@@ -94,37 +94,12 @@ const App = () => {
       />
     );
   }, []);
-  const createIcons = useCallback((option) => {
-    const icons: {
-      EOSReactIcon: any;
-      name: string;
-    }[] = [];
-    const limit =
-      EOSIconsList[option].length < 10 ? EOSIconsList[option].length : 10;
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < limit; i++) {
-      const icon = EOSIconsList[option][i];
-      const name = EOSIcons[`Eos_${icon.name}_Filled`.toUpperCase()]
-        ? `Eos_${icon.name}_Filled`.toUpperCase()
-        : `Eos_${icon.name}_Outlined`.toUpperCase();
-      const EOSReactIcon = EOSIcons[name];
-      if (EOSReactIcon) {
-        icons.push({ EOSReactIcon, name });
-      }
-    }
-
-    return (
-      <IconBox
-        option={option}
-        icons={icons}
-        key={option}
-        pushToNode={pushToNode}
-      />
-    );
-  }, []);
 
   useEffect(() => {
-    updateIcons(OptionsList.map((option) => createIcons(option)));
+    const iconList = OptionsList.map((option) =>
+      searchIconsByName("", searchTheme.current.value, option)
+    );
+    updateIcons(iconList);
   }, []);
 
   const onSearch = useCallback(() => {
